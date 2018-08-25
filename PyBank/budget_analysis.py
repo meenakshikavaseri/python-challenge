caf3947def74610896d77e10266f527cd8c16daf
Meena <sreerc2002@yahoo.com>
2018-08-25 08:41:01 -0400

    HW: almost final draft

--- PyBank/budget_analysis.py
+++ PyBank/budget_analysis.py
@@ -53,6 +53,36 @@ print (counter)
 avg_change=changeP_Lamt/(counter-1)
 print(avg_change)
 print("The average change in Profit/Losses between months over the entire period = $" + str(avg_change))
-#The greatest increase in profits (date and amount) over the entire period 
 
+
+#The greatest increase in profits (date and amount) over the entire period 
+Current_Val=0
+Colsnum=0
+priormonthP_L=0
+maxvalue=0
+diff=0
+MaxValueDate=""
+with open(csvpath, newline="") as csvfile:
+   reader = csv.reader(csvfile,delimiter=',')
+   csv_header = next(reader)
+   for Cols in reader:
+    Current_Val=int(Cols[1])
+    if(Colsnum == 0):
+        priormonthP_L=Current_Val
+    else:
+        diff = Current_Val - priormonthP_L 
+        priormonthP_L= Current_Val
+        
+        if (diff> 0):
+           if(maxvalue == 0):
+            maxvalue=diff
+            MaxValueDate= Cols[0]
+           else:
+                if(diff > maxvalue):
+                    maxvalue = diff
+                    MaxValueDate=Cols[0]
+    Colsnum = Colsnum + 1                
+print(maxvalue)
+print(MaxValueDate)
+print("The greatest increase in profits date =" + MaxValueDate + " and amount $" + str(maxvalue) +" over the entire period" )
 #The greatest decrease in losses (date and amount) over the entire period
