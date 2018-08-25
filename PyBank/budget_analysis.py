d61dfd732f691ecb79295519df25cf53fdc5a8f7
Meena <sreerc2002@yahoo.com>
2018-08-25 09:09:35 -0400

    HW - PyBank - pre-final draft - all exercises completed. export pending

--- PyBank/budget_analysis.py
+++ PyBank/budget_analysis.py
@@ -6,7 +6,7 @@ import os
 import csv
 
 csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
-print(csvpath)
+#print(csvpath)
 
 #set the counter
 counter = -1
@@ -18,7 +18,7 @@ with open(csvpath, newline="") as csvfile:
    for Cols in reader:
     mydate=Cols[0]
     counter = counter+1
-print("Total number of months incl in the dataset = "+ str(counter))
+#print("Total number of months incl in the dataset = "+ str(counter))
 
 #The total net amount of "Profit/Losses" over the entire period
 totalP_L=0
@@ -28,7 +28,7 @@ with open(csvpath, newline="") as csvfile:
    for Cols in reader:
         myP_L=Cols[1]
         totalP_L=totalP_L+int(Cols[1])
-print ("The total net amount of over the entire period = "+ str(totalP_L))
+#print ("The total net amount of over the entire period = "+ str(totalP_L))
 
 #The average change in "Profit/Losses" between months over the entire period
 changeP_Lamt=0
@@ -39,20 +39,20 @@ with open(csvpath, newline="") as csvfile:
    csv_header = next(reader)
    for Cols in reader:
        Current_Val=int(Cols[1])
-       print(Current_Val)
+       ##print(Current_Val)
        if(Colsnum == 0):
            priormonthP_L=Current_Val
        else:
-           print("values:" + str(Current_Val) + " " + str(Colsnum)+" "+str(priormonthP_L))
+           #print("values:" + str(Current_Val) + " " + str(Colsnum)+" "+str(priormonthP_L))
            changeP_Lamt += Current_Val - priormonthP_L
            priormonthP_L= Current_Val
        
        Colsnum = Colsnum + 1
-print(changeP_Lamt)
-print (counter)
+#print(changeP_Lamt)
+#print (counter)
 avg_change=changeP_Lamt/(counter-1)
-print(avg_change)
-print("The average change in Profit/Losses between months over the entire period = $" + str(avg_change))
+#print(avg_change)
+#print("The average change in Profit/Losses between months over the entire period = $" + str(avg_change))
 
 
 #The greatest increase in profits (date and amount) over the entire period 
@@ -82,7 +82,49 @@ with open(csvpath, newline="") as csvfile:
                     maxvalue = diff
                     MaxValueDate=Cols[0]
     Colsnum = Colsnum + 1                
-print(maxvalue)
-print(MaxValueDate)
-print("The greatest increase in profits date =" + MaxValueDate + " and amount $" + str(maxvalue) +" over the entire period" )
+#print(maxvalue)
+#print(MaxValueDate)
+#print("The greatest increase in profits date =" + MaxValueDate + " and amount $" + str(maxvalue) +" over the entire period" )
+
+
 #The greatest decrease in losses (date and amount) over the entire period
+Current_Val=0
+Colsnum=0
+priormonthP_L=0
+minvalue=0
+diff=0
+MinValueDate= ""
+
+with open(csvpath, newline="") as csvfile:
+   reader = csv.reader(csvfile,delimiter=',')
+   csv_header = next(reader)
+
+   for Cols in reader:
+    Current_Val=int(Cols[1])
+    if(Colsnum ==0):
+        priormonthP_L=Current_Val
+    else:
+        diff = Current_Val - priormonthP_L 
+        priormonthP_L= Current_Val
+        
+        if (diff < 0):
+           if(minvalue == 0):
+                minvalue = diff
+                MinValueDate = Cols[0]
+           else:
+                if(diff < minvalue):
+                    minvalue = diff
+                    MinValueDate=Cols[0]
+    Colsnum = Colsnum + 1 
+   
+#print(minvalue)
+#print(MinValueDate)
+
+print("Financial Analysis")
+print("----------------------------")
+print("Total Months: " + str(Colsnum))
+print("Total: $" + str(totalP_L))
+print("Average  Change: $" + str(avg_change))
+print("Greatest Increase in Profits: "  + MaxValueDate + " $(" + str(maxvalue) + ")")
+print("Greatest Decrease in Profits: " +  MinValueDate + " $(" + str(minvalue) + ")")
+
